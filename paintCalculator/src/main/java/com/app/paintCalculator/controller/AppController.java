057227e4b29bfd0bbdabaa1b929e29177ca9fd6a
maruthisundaram <maruthishanmugam7@gmail.com>
2023-07-24 19:06:02 +0530

    Added Swagger doc

--- paintCalculator/src/main/java/com/app/paintCalculator/controller/AppController.java
+++ paintCalculator/src/main/java/com/app/paintCalculator/controller/AppController.java
@@ -3,6 +3,7 @@ package com.app.paintCalculator.controller;
 import com.app.paintCalculator.records.RoomDetails;
 import com.app.paintCalculator.response.PaintResponse;
 import com.app.paintCalculator.service.PaintService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,11 +19,13 @@ public class AppController {
     @Autowired
     private PaintService paintService;
 
+    @Operation(summary = "Greet User", description = "Just welcoming the User while loading the app")
     @GetMapping
     public String greeting(){
         return "Hello Painter";
     }
 
+    @Operation(summary = "Estimate Paint Quantity and Cost", description = "POST API to estimate the amount of paint required for a room and its associated cost")
     @PostMapping("/paintEstimate")
     public ResponseEntity<PaintResponse> estimatePaint(@Valid @RequestBody RoomDetails roomDetails){
         return new ResponseEntity<>(paintService.estimatePaint(roomDetails), HttpStatus.OK);
